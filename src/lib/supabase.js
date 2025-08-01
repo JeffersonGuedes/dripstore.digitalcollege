24cb2c8f9f1014477f6a7deffa985dd3a1d8836f
Jefferson da Silva Guedes <jefferson.guedes@sepog.fortaleza.ce.gov.br>
2025-08-01 13:58:44 -0300

    :bug: Fix: Ajustando variavel de ambiente

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qpqiuwagguutnlcvybqy.supabase.co'
-const supabaseKey = process.env.supabaseKey
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 export const signIn = async (email, password) => {
